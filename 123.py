14e788381db61ba0db17f6b98a308987bfab378f
shaohuan <18368387155@163.com>
2019-08-19 17:58:27 +0800

    测试提交

--- 123.py
+++ 123.py
@@ -1,1 +1,11 @@
-12345
+from selenium import webdriver
+import time
+import unittest
+
+
+try:
+    driver.find_element_by_xpath("xxx x").is_displayed()
+except:
+    print("未找到元素")
+else:
+    print("元素存在")
