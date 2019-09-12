e903fd1abf79b1d973cb657b7d11c55e01814b8a
liuchen <18368387155@163.com>
2019-09-12 11:46:39 +0800

    测试提交

--- 123/test.py
+++ 123/test.py
@@ -1,26 +1,18 @@
-import unittest
 from selenium import webdriver
 import time
 
-class LearnElement(unittest.TestCase):
-    #初始化，打开浏览器
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get('http://www.baidu.com')
+brower = webdriver.Firefox()
+# brower.get("http://www.baidu.com")
+#
+# brower.find_element_by_id('kw').send_keys('selenium')
+# brower.find_element_by_id('su').click()
+brower.get("https://magicube.moredian.com/#/login")
+time.sleep(2)
+# brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[3]/div/div[2]/div/input').click()
+brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[3]/div/div[2]/div/input').send_keys('18368387155')
+# brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[4]/div/div[2]/div/input').click()
+brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[4]/div/div[2]/div/input').send_keys('Md123456')
+brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[5]/div').click()
 
-    def testXpath(self):
-        #定位百度首页的输入框，绝对路径定位成功
-        element = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/form/span/input')
-        #定位百度首页的输入框，相对路径
-        element = self.driver.find_element_by_xpath("//input[@id='kw']")
-        #定位百度首页的输入框，相对路径下的布尔逻辑运算
-        element=self.driver.find_element_by_xpath("//input[@id='kw'and @name='wd']")
-        element.send_keys('http://wsbm.sdzk.cn')
-        time.sleep(5)
-
-    #--------关闭浏览器------------
-    #def tearDown(self):
-     #   self.driver.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+time.sleep(2)
+brower.close()
