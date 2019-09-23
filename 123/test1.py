1ae737f42e0d84716520d9ac381c1b19464936ff
liuchen <18368387155@163.com>
2019-09-23 17:55:17 +0800

    测试提交

--- 123/test1.py
+++ 123/test1.py
@@ -1,28 +1,24 @@
+import random
+import time
 import selenium
 from selenium import webdriver
-import time
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 brower = webdriver.Firefox()
-brower.implicitly_wait(3)
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-
-
-
+brower.implicitly_wait(2)
 
 # 打开网页
 brower.get("https://magicube.moredian.com/#/login")
-# driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-# time.sleep(2)#等待网页加载
+
 # 登陆
-# brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[3]/div/div[2]/div/input').click()
+brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[3]/div/div[2]/div/input').click()
 brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[3]/div/div[2]/div/input').send_keys('18368387155')
 brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[4]/div/div[2]/div/input').click()
 brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[4]/div/div[2]/div/input').send_keys('Md123456')
 brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[2]/div[5]/div').click()
 print("1、登陆成功")
-# time.sleep(2)
 
 # 跳转我的机构
 brower.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[3]/i[2]').click()
@@ -30,12 +26,29 @@ brower.find_element_by_xpath('/html/body/div[3]/ul/li[3]').click()
 
 # 确认是否满机构
 try:
-    brower.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/table/tr[15]/td[1]/div')
+    # brower.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/table/tr[15]/td[1]/div').click()
+    brower.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/table/tr[15]/td[1]/div')
     print("15个机构已满")
-    brower.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/span').click()
-    if brower.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/div[2]/span').text == "所在机构数量达到上限":
-        brower.find_element_by_xpath('/html/body/div[3]/div/div/div[2]/div/span').click()
+
 except selenium.common.exceptions.NoSuchElementException:
-    brower.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/span').click()
+    #新增机构
+    brower.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div/div/div[1]/div[2]/div[1]').click()
+    brower.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div/div[2]/div[1]/div/div[1]/div[1]/div[2]/div/input').click()
+    # num = range(1,99999999)
+    brower.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div/div[2]/div[1]/div/div[1]/div[1]/div[2]/div/input').send_keys(random.randint(100000,999999))
+    brower.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div/div[2]/div[1]/div/div[2]/div[1]/div[2]/div/div[1]/span').click()
+    brower.find_element_by_xpath('//*[@id="search-input"]').click()
+    brower.find_element_by_xpath('//*[@id="search-input"]').send_keys("北京")
+    try:
+        element = WebDriverWait(brower, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="amap-sug1"]')))
+    finally:
+        brower.find_element_by_xpath('//*[@id="amap-sug1"]').click()
+    brower.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div/div[2]/div/div[2]/div[2]/div[1]/div/div/div[1]/ul/li[1]/div[1]').click()
+    brower.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div/div[1]').click()   # 保存地址
+    time.sleep(2)
+    # brower.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div/div[1]').click() # 创建机构
+    brower.find_element_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div/div[2]').click()
+except Exception as e:
+    print(e)
 
 # print(flag)
